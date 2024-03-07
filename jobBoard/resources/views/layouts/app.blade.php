29a62345cf672dea51b9d4fe7cd89de291657ea3
besma02 <besmaharzli@gmail.com>
2024-03-07 08:31:19 +0100

    link company

--- jobBoard/resources/views/layouts/app.blade.php
+++ jobBoard/resources/views/layouts/app.blade.php
@@ -19,7 +19,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-       
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{route('check.login')}}">Company</a>
+        </li>
         
         @if(!Auth::check())
         <li class="nav-item">
